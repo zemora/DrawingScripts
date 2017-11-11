65fdacf19e30203f172968bb16667d1b3a3c553b
zemora <xidalapuda@126.com>
2017-11-12 00:52:41 +0800

    Update planepartition.py

--- planepartition.py
+++ planepartition.py
@@ -7,6 +7,16 @@ TOP_COLOR = (1, 0, 0)
 LEFT_COLOR = (0, 1, 1)
 RIGHT_COLOR = (0.75, 0.5, 0.25)
 
-LEFT = np.exp(1j * np.pi * 7 / 6)
-TOP = np.exp(1j * np.pi / 2)
-RIGHT = np.exp(np.pi * 11 / 6 * 1j)
+X = np.exp(1j * np.pi * 7 / 6)
+Z = np.exp(1j * np.pi / 2)
+Y = np.exp(np.pi * 11 / 6 * 1j)
+
+TOP = np.array([(0, 0),
+                (np.sqrt(3) * 0.5, 0.5),
+                (0, 1),
+                (-np.sqrt(3) * 0.5, 0.5),
+                (0, 0)])
+
+LEFT = np.array([(0, 0),
+                 (-np.sqrt(3) * 0.5, 0.5),
+                 ()])
