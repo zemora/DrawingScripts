b12113ee4f3c598f1cd283a5e0b71ebaf9f24f4b
zemora <xidalapuda@126.com>
2017-11-12 00:44:32 +0800

    Update planepartition.py

--- planepartition.py
+++ planepartition.py
@@ -3,4 +3,10 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 
+TOP_COLOR = (1, 0, 0)
+LEFT_COLOR = (0, 1, 1)
+RIGHT_COLOR = (0.75, 0.5, 0.25)
 
+LEFT = np.exp(1j * np.pi * 7 / 6)
+TOP = np.exp(1j * np.pi / 2)
+RIGHT = np.exp(np.pi * 11 / 6 * 1j)
