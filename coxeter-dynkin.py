0e57e9eeb29a10728f3de91528fdcc50c284997a
此Repo仅供测试用 <xidalapuda@126.com>
2018-09-19 15:16:57 +0800

    Add files via upload

--- coxeter-dynkin.py
+++ coxeter-dynkin.py
@@ -1,20 +1,67 @@
-import matplotlib.pyplot as plt
+from piscript.PiModule import *
+from math import *
 
-def coxeter_dynkin(diagram, active):
-    nodes = len(diagram) + 1
-    intervals = len(diagram)
-    fig = plt.figure(figsize=(intervals + 0.4, 0.6), dpi=100)
-    ax = fig.add_axes([0, 0, 1, 1], aspect=1)
-    ax.axis("off")
-    ax.axis([0.8, nodes + 0.2, -0.2, 0.4])
-    ax.plot([1, nodes], [0, 0], "k-")
-    ax.plot(list(range(1, nodes+1)), [0]*nodes, "ko", markersize=6)
-    for i, x in enumerate(active, start=1):
-        if x:
-            ax.plot([i], [0], "ko", markersize=12, markerfacecolor='none')
-    for i, x in enumerate(diagram):
-        if x > 3:
-            ax.text(i + 1.4, 0.1, str(x), fontsize=16)
-    fig.savefig("".join(str(x) for x in diagram) + ".png")
+init(400, 100)
+center()
+scale(100)
 
-coxeter_dynkin((5, 3, 3), (0, 1, 0,1))
+ex = 1.6
+r = 0.08
+setlinewidth(2.5)
+
+
+newpath()
+circle(0, 0, r)
+fill(1, 1, 0)
+stroke()
+
+newpath()
+circle(-ex, 0, r)
+fill(1, 1, 0)
+stroke()
+
+newpath()
+circle(ex, 0, r)
+fill(1, 1, 0)
+stroke()
+
+newpath()
+circle(-ex, 0, 2*r)
+stroke()
+
+t = texinsert(r"${\bf 4}$")
+t.scale(3)
+t.translate(-t.width, -t.height/2)
+place(t, -ex/2, 0.2)
+
+"""
+newpath()
+circle(0, 0, 2*r)
+stroke()
+
+newpath()
+circle(ex, 0, 2*r)
+stroke()
+
+
+newpath()
+moveto(-ex+2*r, 0)
+lineto(-2*r, 0)
+stroke()
+
+newpath()
+moveto(2*r, 0)
+lineto(ex-2*r, 0)
+stroke()
+"""
+newpath()
+moveto(-ex+2*r, 0)
+lineto(-r, 0)
+stroke()
+
+newpath()
+moveto(r, 0)
+lineto(ex-r, 0)
+stroke()
+
+finish()
