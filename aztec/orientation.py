d979c203e2b198f32019305be92301248ab3d18e
此Repo仅供测试用 <xidalapuda@126.com>
2018-09-29 14:23:28 +0800

    Add files via upload

--- aztec/orientation.py
+++ aztec/orientation.py
@@ -10,7 +10,6 @@ sw = 0.045
 hw = 5 * sw
 setarrowdims(sw, hw)
 
-
 gray = 0
 
 def rectangle(x1, y1, x2, y2, gray):
@@ -26,7 +25,7 @@ def rectangle(x1, y1, x2, y2, gray):
 space1 = 1
 space2 = 2
 space3 = space1
-ht = 0.3
+ht = 0.4
 nx = -6
 sx = nx + 2 + space1
 wx = sx + 2 + space2
@@ -38,7 +37,7 @@ rectangle(nx+1, 0, nx+2, 1, gray)
 
 newpath()
 arrow((nx+1, 1+ht), (nx+1, 2+ht))
-fill(1, 0, 1)
+fill(1)
 scalelinewidth(0.5)
 stroke()
 
@@ -50,7 +49,7 @@ rectangle(sx+1, 0, sx+2, 1, 1)
 
 newpath()
 arrow((sx+1, -ht), (sx+1, -1-ht))
-fill(1, 0, 1)
+fill(1)
 scalelinewidth(0.5)
 stroke()
 
@@ -60,7 +59,7 @@ rectangle(wx, 0.5, wx+1, 1.5, 1)
 rectangle(wx, -0.5, wx+1, 0.5, gray)
 newpath()
 arrow((wx-ht, 0.5), (wx-ht-1, 0.5))
-fill(1, 0, 1)
+fill(1)
 scalelinewidth(0.5)
 stroke()
 #----------------------
@@ -69,7 +68,7 @@ rectangle(ex, 0.5, ex+1, 1.5, 1)
 rectangle(ex, -0.5, ex+1, 0.5, gray)
 newpath()
 arrow((ex+1+ht, 0.5), (ex+2+ht, 0.5))
-fill(1, 0, 1)
+fill(1)
 scalelinewidth(0.5)
 stroke()
 
