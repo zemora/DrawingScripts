2a7e3530d25724d085c64aef3a7fcb3cbf3b411c
此Repo仅供测试用 <xidalapuda@126.com>
2021-05-12 08:10:26 +0800

    Add files via upload

--- dpp/macdonald-bijection.py
+++ dpp/macdonald-bijection.py
@@ -1,9 +1,9 @@
 from piscript.PiModule import *
 
 
-init("macdonald-bijection.eps", 400, 340, "noclip")
+init("macdonald-bijection.eps", 320, 220, "noclip")
 scale(40)
-translate(1, 1)
+translate(.6, .6)
 setarrowdims(0.08, 0.15)
 
 ts = 1
@@ -12,8 +12,8 @@ ts = 1
 
 gsave()
 setdash([6, 6], 0)
-N = 8
-M = 6
+N = 6
+M = 4
 for k in range(0, N+1):
     moveto(k, 0)
     lineto(k, M)
@@ -25,14 +25,15 @@ for k in range(0, M+1):
 grestore()
 
 newpath()
-arrow([8, 0], [8.5, 0])
+arrow([6, 0], [6.5, 0])
 stroke()
 
 newpath()
-arrow([0, 6], [0, 6.5])
+arrow([0, 4], [0, 4.5])
 stroke()
 
 scalelinewidth(1.5)
+
 newpath()
 moveto(0, 0)
 lineto(1, 0)
@@ -41,34 +42,65 @@ lineto(3, 1)
 lineto(3, 2)
 lineto(6, 2)
 lineto(6, 4)
-lineto(7, 4)
-lineto(7, 5)
-lineto(7, 6)
-lineto(8, 6)
+lineto(0, 4)
+closepath()
 stroke()
 
+newpath()
+moveto(1, 0)
+lineto(1, 4)
+stroke()
+
+newpath()
+moveto(2, 1)
+lineto(2, 4)
+stroke()
+
+
+newpath()
+moveto(3, 1)
+lineto(3, 4)
+stroke()
+
+
+newpath()
+moveto(4, 2)
+lineto(4, 4)
+stroke()
+
+
+newpath()
+moveto(5, 2)
+lineto(5, 4)
+stroke()
+
+newpath()
+moveto(6, 2)
+lineto(6, 4)
+stroke()
 
 
 t = texinsert("$x$")
 t.translate(-t.width/2, 0)
 t.scale(ts)
-place(t, 8.5, -0.5)
+place(t, 6, -0.3)
 
 t = texinsert("$y$")
 t.translate(-t.width/2, 0)
 t.scale(ts)
-place(t, -0.4, 6.5)
+place(t, -0.3, 4)
 
 t = texinsert("$O$")
 t.translate(-t.width/2, 0)
 t.scale(ts)
-place(t, -0.5, -0.5)
+place(t, -0.3, -0.3)
 
-t = texinsert("$(m,n)$")
+t = texinsert("$(r-k+1,\lambda_k)$")
 t.translate(-t.width/2, 0)
 t.scale(1)
-place(t, 8+0.2, 6+0.2)
+place(t, 6+0.2, 4+0.2)
 
+"""
 t = texinsert("$\lambda=(\lambda_1,\lambda_2,\ldots,\lambda_6)=(7, 7, 6, 6, 3, 1)$")
 t.translate(-t.width/2, 0)
 t.scale(ts)
@@ -79,21 +111,11 @@ t = texinsert(r"$\{\lambda_i+n-i\ |\ 1\leq i\leq 6\}=\{12, 11, 9, 8, 4, 1\}$")
 t.translate(-t.width/2, 0)
 t.scale(ts)
 place(t, 4, 6+0.5)
-
-
-t = texinsert("$m=8$")
-t.translate(-t.width/2, 0)
-t.scale(1)
-place(t, 4, -0.5)
-
-t = texinsert("$n=6$")
-t.translate(-t.width/2, -t.height/2)
-t.scale(1)
-place(t, -0.5, 3)
+"""
 
 ts = 1
 k = -0.3
-setcolor(0.8, 0, 0)
+setcolor(0, 0.5, 1)
 t = texinsert("0")
 t.translate(-t.width/2, 0)
 t.scale(ts)
@@ -124,18 +146,10 @@ t.translate(-t.width/2, 0)
 t.scale(ts)
 place(t, 5.5, k+2)
 
-t = texinsert("10")
-t.translate(-t.width/2, 0)
-t.scale(ts)
-place(t, 6.5, k+4)
-
-t = texinsert("13")
-t.translate(-t.width/2, 0)
-t.scale(ts)
-place(t, 7.5, k+6)
 
 
-setcolor(0, 0.8, 0)
+
+setcolor(1, 0.5, 0)
 j = 0.2
 t = texinsert("1")
 t.translate(-t.width/2, -t.height/2)
@@ -157,14 +171,35 @@ t.translate(-t.width/2, -t.height/2)
 t.scale(ts)
 place(t, j+6, 3.5)
 
-t = texinsert("11")
+
+setcolor(0)
+k = 0.3
+t = texinsert("$\lambda_k$")
 t.translate(-t.width/2, -t.height/2)
 t.scale(ts)
-place(t, j+7, 4.5)
+place(t, 0.5, k)
 
-t = texinsert("12")
+t = texinsert("$\lambda_{k+1}$")
 t.translate(-t.width/2, -t.height/2)
 t.scale(ts)
-place(t, j+7, 5.5)
+place(t, 1.5, 1+k)
 
+t = texinsert("$\lambda_{r}$")
+t.translate(-t.width/2, -t.height/2)
+t.scale(ts)
+place(t, 5.5, 2+k)
+
+rad = 0.08
+"""
+for x, y in [(1, 0), (3, 1), (6, 2), (6, 3)]:
+    newpath()
+    circle(x, y, rad)
+    fill(1, 0.5, 0)
+    stroke()
+"""
+for x, y in [(1, 0), (2, 1), (3, 1), (4, 2), (5, 2), (6, 2)]:
+    newpath()
+    circle(x, y, rad)
+    fill(0, 0.5, 1)
+    stroke()
 finish()
