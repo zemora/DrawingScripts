04870a3463b02adea01695f2a4ca4dc1cd9a1a01
此Repo仅供测试用 <xidalapuda@126.com>
2021-11-17 09:56:55 +0800

    Update cftp_hexagon.py

--- cftp_hexagon.py
+++ cftp_hexagon.py
@@ -53,7 +53,25 @@ def draw_background_triangle_lattice(ax, ULR, n):
     return lines
 
 
-def draw_paths_on_hexagon(ax, size, ULR, paths):
+def draw_background_square_lattice(ax, ULR, n):
+    """
+    Draw the background triangle lattice in a hexagon (dashed gray lines).
+    """
+    U, L, R = ULR
+    k = np.sqrt(2)
+    lines = []
+    for i in range(-n, n):
+        # two families of grid lines
+        for P, Q in [[U, L], [U, R]]:
+            v1 = i * P * k + n * Q
+            v2 = i * P * k - n * Q
+            lines.extend(
+                plotline(ax, v1, v2, '--', color=(0.5, 0.5, 0.5), lw=1)
+            )
+    return lines
+
+
+def draw_triangle_paths_on_hexagon(ax, size, ULR, paths):
     """
     Draw a set of nonintersecting paths on a matplotlib `ax` instance (bold red).
     """
@@ -62,9 +80,27 @@ def draw_paths_on_hexagon(ax, size, ULR, paths):
     for i, path in enumerate(paths[1:-1]):
         v1 = b * L + (i + 0.5) * U
         for ht1, ht2 in zip(path[:-1], path[1:]):
-            if ht1 < ht2:
+            if ht1 < ht2:  # move right-up
                 v2 = v1 - L
-            else:
+            else:  # move right-down
+                v2 = v1 + R
+            plotline(ax, v1, v2, 'r-', lw=2)
+            v1 = v2
+
+
+def draw_square_paths_on_hexagon(ax, size, ULR, paths):
+    """
+    Draw a set of nonintersecting paths on a matplotlib `ax` instance (bold red).
+    """
+    a, b, c = size
+    U, L, R = ULR
+    k = np.sqrt(2)
+    for i, path in enumerate(paths[1:-1]):
+        v1 = b * L + i * U * k
+        for ht1, ht2 in zip(path[:-1], path[1:]):
+            if ht1 < ht2:  # move right-up
+                v2 = v1 - L
+            else:  # move right-down
                 v2 = v1 + R
             plotline(ax, v1, v2, 'r-', lw=2)
             v1 = v2
@@ -88,7 +124,7 @@ def draw_hexagon_on_triangle_lattice(size, paths=None):
 
     fig = plt.figure(figsize=(6, 6), dpi=100)
     ax = fig.add_axes([0, 0, 1, 1], aspect=1)
-    ax.axis([-b * k - 0.5, a * k + 0.5, -(a + b) / 2 - 0.5, c + 0.5])
+    ax.axis([-b * k - 1, a * k + 1, -(a + b) / 2 - 1, c + 1])
     ax.axis("off")
 
     verts = [A, A + B, B, B + C, C, A + C, A]
@@ -99,7 +135,7 @@ def draw_hexagon_on_triangle_lattice(size, paths=None):
         line.set_clip_path(patch)
 
     if paths:
-        draw_paths_on_hexagon(ax, size, (U, L, R), paths)
+        draw_triangle_paths_on_hexagon(ax, size, (U, L, R), paths)
 
     # add text annotations
     size = 18
@@ -112,4 +148,40 @@ def draw_hexagon_on_triangle_lattice(size, paths=None):
     fig.savefig("hexagon.png")
 
 
-draw_hexagon_on_triangle_lattice((5, 4, 6))
+def draw_hexagon_on_square_lattice(size, paths=None):
+    """
+    Set up the figure and combine all the stuff.
+    :param size: side lengths of the hexagon, a tuple of three integers.
+    :param paths: a set of nonintersecting paths.
+    """
+    U = dir(90)
+    L = dir(225)
+    R = dir(-45)
+
+    a, b, c = size
+    k = np.sqrt(2)
+    A = (c - 1) * U * k
+    B = b * L
+    C = a * R
+
+    fig = plt.figure(figsize=(6, 6), dpi=100)
+    ax = fig.add_axes([0, 0, 1, 1], aspect=1)
+    ax.axis([-b / k - 1, a / k + 1, -(a + b) / k - 1, (c - 1) * k + 1])
+    ax.axis("off")
+
+    verts = [A, A + B, B, B + C, C, A + C, A]
+    n = 2 * (a + b + c)
+    patch = draw_bounding_hexagon(ax, verts)
+    lines = draw_background_square_lattice(ax, (U, L, R), n)
+    for line in lines:
+        line.set_clip_path(patch)
+
+    if paths:
+        draw_square_paths_on_hexagon(ax, size, (U, L, R), paths)
+
+    fig.savefig("square.png")
+
+
+size = (12, 10, 6)
+draw_hexagon_on_triangle_lattice(size)
+draw_hexagon_on_square_lattice(size)
