7286e2ed94ef23fd4a0f3ec6449cfa07638a829f
此Repo仅供测试用 <xidalapuda@126.com>
2021-11-17 09:27:08 +0800

    Update cftp_hexagon.py

--- cftp_hexagon.py
+++ cftp_hexagon.py
@@ -21,7 +21,7 @@ def plotline(ax, z1, z2, *args, **kwargs):
 
 def draw_bounding_hexagon(ax, vertices):
     """
-    Draw the bounding hexagon on a `ax` instance.
+    Draw the bounding hexagon on a `ax` instance and return the path patch.
     """
     coords = [(z.real, z.imag) for z in vertices]
     xlist, ylist = zip(*coords)
@@ -63,26 +63,25 @@ def draw_paths_on_hexagon(ax, size, ULR, paths):
             v1 = v2
 
 
-def draw_hexagon_on_triangle_lattice(size, s=None):
+def draw_hexagon_on_triangle_lattice(size, paths=None):
     """
     Draw hexagon on the triangle lattice.
     :param a, b, c: side lengths of the hexagon.
-    :param s: a state in the sampling Markov chain, it's indeed a tiling
-        represented as a set of nonintersecting paths.
+    :param paths: a set of nonintersecting paths.
     """
     U = dir(90)
     L = dir(210)
     R = dir(-30)
 
     a, b, c = size
-    K = np.sqrt(3) / 2
+    k = np.sqrt(3) / 2
     A = c * U
     B = b * L
     C = a * R
 
     fig = plt.figure(figsize=(6, 6), dpi=100)
     ax = fig.add_axes([0, 0, 1, 1], aspect=1)
-    ax.axis([-b * K - 0.5, a * K + 0.5, -(a + b) / 2 - 0.5, c + 0.5])
+    ax.axis([-b * k - 0.5, a * k + 0.5, -(a + b) / 2 - 0.5, c + 0.5])
     ax.axis("off")
 
     verts = [A, A + B, B, B + C, C, A + C, A]
@@ -92,13 +91,8 @@ def draw_hexagon_on_triangle_lattice(size, s=None):
     for line in lines:
         line.set_clip_path(patch)
 
-    s0 = [
-        [max(j - a, 0) if k == 0 else k + min(j, b) for j in range(a + b + 1)]
-        for k in range(c + 2)
-    ]
-    print(s0)
-
-    draw_paths_on_hexagon(ax, size, (U, L, R), s0)
+    if paths:
+        draw_paths_on_hexagon(ax, size, (U, L, R), paths)
 
     size = 18
     z = A + C / 2
